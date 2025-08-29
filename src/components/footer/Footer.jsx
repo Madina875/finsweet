eb248445c728ce28fa8418e27052aca58de4cb04
madina875 <evaevasmq@gmail.com>
2025-08-29 11:13:52 +0500

    hmw

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,50 @@
+import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
 import React from "react";
+import headerLogo from "../../assets/logo.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-[#1C1E53] h-[489px]">
+      <div className="container flex place-items-center h-[100%]">
+        <div className="wrapper flex w-[90%] gap-[140px] ">
+          <div className="left text-white  flex-[40%] flex flex-col gap-4">
+            <img
+              src={headerLogo}
+              alt="header-logo"
+              className="size-[30%] pt-5"
+            />
+            <p className="w-[70%]">
+              We are always open to discuss your project and improve your online
+              presence.
+            </p>
+            <div className="text-black bg-[#FCD980] flex gap-[30px] mt-[60px] pt-[17px] pl-[37px] pb-[17px] w-[80%]">
+              <div>
+                <span className="font-medium">Email me at</span>
+                <p>contact@website.com</p>
+              </div>
+              <div>
+                <span className="font-medium">Call us</span>
+                <p> 0927 6277 28525</p>
+              </div>
+            </div>
+          </div>
+          <div className="right text-white flex-[30%] flex-col">
+            <p className="text-[48px] font-medium mb-2">Lets Talk!</p>
+            <p className="text-gray-400">
+              We are always open to discuss your project, improve your online
+              presence and help with your UX/UI design challenges.
+            </p>
+            <div className="flex gap-[27px] mt-7">
+              <Facebook />
+              <Twitter />
+              <Instagram />
+              <Linkedin />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import headerLogo from "../../assets/logo.svg";
 const Header = () => {
   return (
-    <header>
-      <div></div>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/pricing">Pricing</Link>
-        <Link to="/work">Work</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/contact">Contact</Link>
-      </ul>
+    <header className="bg-[#1C1E53] grid place-items-center h-[90px]">
+      <div className="container flex justify-between text-white place-items-center">
+        <div>
+          <Link to="/">
+            <img src={headerLogo} alt="header-logo" />
+          </Link>
+        </div>
+        <ul className="flex gap-[40px] text-[#BBBBCB]">
+          <Link className="hover:text-white" to="/">
+            Home
+          </Link>
+          <Link className="hover:text-white" to="/about">
+            About
+          </Link>
+          <Link className="hover:text-white" to="/pricing">
+            Pricing
+          </Link>
+          <Link className="hover:text-white" to="/work">
+            Work
+          </Link>
+          <Link className="hover:text-white" to="/blog">
+            Blog
+          </Link>
+          <button>
+            <Link
+              to="/contact"
+              className="pt-4 pb-4 pl-12 pr-12 border place-items-center rounded-[41px] hover:text-white"
+            >
+              Contact us
+            </Link>
+          </button>
+        </ul>
+      </div>
     </header>
   );
 };
